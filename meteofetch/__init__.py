c1b47c0ef15e3e5d96d8d0f4dda198c97bd2ce41
CyrilJl <joly.cyril7@gmail.com>
2025-03-28 20:07:40 +0100

    Set ECCODES_DEFINITION_PATH environment variable and reload cfgrib and xarray modules

--- meteofetch/__init__.py
+++ meteofetch/__init__.py
@@ -1,3 +1,15 @@
+import importlib
+import os
+import sys
+from pathlib import Path
+
+os.environ["ECCODES_DEFINITION_PATH"] = str(Path(__file__).parent / "gribdefs")
+
+if "cfgrib" in sys.modules:
+    importlib.reload(sys.modules["cfgrib"])
+    importlib.reload(sys.modules["xarray"])
+
+
 from ._arome import (
     Arome001,
     Arome0025,
