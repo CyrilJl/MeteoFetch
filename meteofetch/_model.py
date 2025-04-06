f6fb1295fceefa27283dd5565cfca3666adf0d73
CyrilJl <joly.cyril7@gmail.com>
2025-04-06 18:00:06 +0200

    Refactor file download logic to use TemporaryDirectory and improve error handling; streamline data processing in _download_paquet method

--- meteofetch/_model.py
+++ meteofetch/_model.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -25,29 +24,27 @@ class Model:
     @classmethod
     def _download_file(cls, url: str, variables: List[str]) -> List[xr.DataArray]:
         try:
-            with requests.get(url=url, timeout=cls.TIMEOUT) as response:
-                response.raise_for_status()
-                with TemporaryDirectory(prefix="meteofetch_") as tmp_dir:
-                    file_path = Path(tmp_dir) / "data.grib2"
-                    with open(file_path, "wb") as f:
-                        f.write(response.content)
-                    datasets = cfgrib.open_datasets(
-                        path=file_path,
-                        indexpath="",
-                        decode_timedelta=True,
-                    )
-                    if variables is not None:
-                        for ds in datasets:
-                            ds = ds.drop_vars([var for var in ds.data_vars if var not in variables])
-                    ret = []
-                    for _ in range(len(datasets)):
-                        ds = datasets.pop()
-                        for var in ds.data_vars:
-                            ret.append(ds[var].compute())
-                    return ret
+            with TemporaryDirectory() as tempdir:
+                temp_path = Path(tempdir) / "temp.grib"
 
-        except Exception as e:
-            raise requests.HTTPError(f"Erreur lors du téléchargement du fichier : {e}")
+                response = requests.get(url, timeout=cls.TIMEOUT)
+                response.raise_for_status()
+
+                with open(temp_path, "wb") as f:
+                    f.write(response.content)
+                datasets = cfgrib.open_datasets(temp_path, backend_kwargs={"decode_timedelta": True, "indexpath": ""})
+
+                dataarrays = []
+                for ds in datasets:
+                    for var in ds.data_vars:
+                        if variables and var not in variables:
+                            continue
+                        dataarrays.append(ds[var].load())
+
+                return dataarrays
+
+        except Exception:
+            return []
 
     @classmethod
     def _download_paquet(cls, date, paquet, variables, num_workers: int) -> Dict[str, xr.DataArray]:
@@ -60,31 +57,28 @@ class Model:
             cls.base_url_ + "/" + cls.url_.format(date=date, paquet=paquet, group=group) for group in cls.groups_
         ]
 
-        ret = defaultdict(list)
-
+        ret = {}
         with ThreadPoolExecutor(max_workers=num_workers) as executor:
+            future_to_url = {executor.submit(cls._download_file, url, variables_): url for url in urls_to_download}
 
-            def download_task(url):
-                return cls._download_file(url, variables_)
+            for future in as_completed(future_to_url):
+                dataarrays = future.result()
+                for da in dataarrays:
+                    if da.name not in ret:
+                        ret[da.name] = []
+                    ret[da.name].append(cls._process_ds(da))
+                    del da
 
-            results_iterator = executor.map(download_task, urls_to_download)
-            for data_arrays_for_url in results_iterator:
-                for da in data_arrays_for_url:
-                    ret[da.name].append(da)
-
-        for key in ret.keys():
-            ret[key] = xr.concat(ret[key], dim="time")
-            ret[key] = cls._process_ds(ret[key])
-            ret[key] = geo_encode_cf(ret[key])
-            ret[key].attrs["Packaged by"] = "meteofetch"
-
+        for field in ret:
+            ret[field] = xr.concat(ret[field], dim="time")
+            ret[field] = geo_encode_cf(ret[field])
         return ret
 
     @classmethod
     def check_paquet(cls, paquet):
         """Vérifie si le paquet spécifié est valide."""
         if paquet not in cls.paquets_:
-            raise ValueError(f"paquet must be one of {cls.paquets_}")
+            raise ValueError(f"Le paquet doit être un des suivants : {cls.paquets_}")
 
     @classmethod
     def get_forecast(cls, date, paquet="SP1", variables=None, num_workers: int = 4) -> Dict[str, xr.DataArray]:
@@ -105,16 +99,15 @@ class Model:
 
         for k in range(cls.past_runs_):
             current_date = latest_possible_date - pd.Timedelta(hours=cls.freq_update * k)
-            try:
-                return cls.get_forecast(
-                    date=current_date,
-                    paquet=paquet,
-                    variables=variables,
-                    num_workers=num_workers,
-                )
-            except Exception as _:
-                pass
-        raise requests.HTTPError(f"Aucun paquet n'a été trouvé parmi les derniers {cls.past_runs_} runs.")
+            datasets = cls.get_forecast(
+                date=current_date,
+                paquet=paquet,
+                variables=variables,
+                num_workers=num_workers,
+            )
+            if datasets:
+                return datasets
+        raise requests.HTTPError(f"Aucun paquet n'a été trouvé parmi les {cls.past_runs_} derniers runs.")
 
 
 def common_process(ds: xr.Dataset) -> xr.Dataset:
@@ -125,15 +118,12 @@ def common_process(ds: xr.Dataset) -> xr.Dataset:
         keep_attrs=True,
     )
     ds = ds.sortby("longitude").sortby("latitude")
-
-    if "time" in ds.dims:
-        ds = ds.sortby("time")
     return ds
 
 
 class HourlyProcess:
     @staticmethod
-    def _process_ds(ds) -> xr.Dataset:
+    def _process_ds(ds: xr.DataArray) -> xr.DataArray:
         ds = ds.expand_dims("valid_time").drop_vars("time").rename(valid_time="time")
         ds = common_process(ds)
         return ds
@@ -141,8 +131,8 @@ class HourlyProcess:
 
 class MultiHourProcess:
     @staticmethod
-    def _process_ds(ds) -> xr.Dataset:
-        if "time" in ds:
+    def _process_ds(ds: xr.DataArray) -> xr.DataArray:
+        if "time" in ds.coords:
             ds = ds.drop_vars("time")
         if "step" in ds.dims:
             ds = ds.swap_dims(step="valid_time").rename(valid_time="time")
