4c94ea12c8fa9d499ef8accefffb1768b27046f8
Cyril <joly.cyril7@gmail.com>
2025-04-09 17:34:13 +0200

    Update _model.py

--- meteofetch/_model.py
+++ meteofetch/_model.py
@@ -50,7 +50,7 @@ class Model:
                         if variables and var not in variables:
                             continue
                         if os.environ.get("meteofetch_test_mode") == "1":
-                            dataarrays.append(ds.isnull())
+                            dataarrays.append(dataarrays.append(ds[var].isnull()))
                         else:
                             dataarrays.append(ds[var].load())
 
