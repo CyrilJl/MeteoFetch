ab286ca87e0c86632bcec6168229ad7019e12cd4
Cyril <joly.cyril7@gmail.com>
2025-08-26 09:21:11 +0000

    availability for ifs and aifs

--- meteofetch/_ecmwf.py
+++ meteofetch/_ecmwf.py
@@ -1,4 +1,3 @@
-from concurrent.futures import ThreadPoolExecutor
 from tempfile import TemporaryDirectory
 from typing import Dict
 
@@ -51,7 +50,7 @@ class ECMWF(Model):
         path: str = None,
         return_data: bool = True,
         num_workers: int = 4,
-    ) -> Dict[str, xr.DataArray] or list:
+    ) -> Dict[str, xr.DataArray] | list:
         """Récupère les prévisions pour une date donnée."""
         date_dt = pd.to_datetime(str(date)).floor(f"{cls.freq_update}h")
         date_str = f"{date_dt:%Y-%m-%dT%H}"
@@ -78,7 +77,7 @@ class ECMWF(Model):
                 return paths
 
     @classmethod
-    def get_latest_forecast_time(cls) -> pd.Timestamp or bool:
+    def get_latest_forecast_time(cls) -> pd.Timestamp | bool:
         """Trouve l'heure de prévision la plus récente disponible parmi les runs récents.
 
         Parcourt les cls.past_runs_ derniers runs dans l'ordre chronologique inverse
@@ -102,7 +101,7 @@ class ECMWF(Model):
         path: str = None,
         return_data: bool = True,
         num_workers: int = 4,
-    ) -> Dict[str, xr.DataArray] or list:
+    ) -> Dict[str, xr.DataArray] | list:
         """Récupère les dernières prévisions disponibles parmi les runs récents."""
         date = cls.get_latest_forecast_time()
         if date:
@@ -118,30 +117,13 @@ class ECMWF(Model):
         raise requests.HTTPError(f"Aucun paquet n'a été trouvé parmi les {cls.past_runs_} derniers runs.")
 
     @classmethod
-    def availability(cls, return_date: bool = False) -> pd.DataFrame or pd.Timestamp:
-        """Retourne la disponibilité des {cls.past_runs_} derniers runs.
-
-        Returns:
-            pd.DataFrame or pd.Timestamp:
-                Si return_date est False (par défaut), retourne un DataFrame avec les dates des runs
-                et un booléen indiquant si le run est complet.
-                Si return_date est True, retourne le timestamp du dernier run complet.
-        """
+    def availability(cls, return_date=False):
         latest_possible_date = pd.Timestamp.now().floor(f"{cls.freq_update}h")
-        dates = [latest_possible_date - pd.Timedelta(hours=cls.freq_update * k) for k in range(cls.past_runs_)]
-
-        availability_info = []
-        for date in dates:
-            urls = cls._get_urls(date=date)
-            availability_info.append({"date": date, "available": are_downloadable(urls)})
-
-        df = pd.DataFrame(availability_info)
-
-        if return_date:
-            available_runs = df[df["available"]]
-            if not available_runs.empty:
-                return available_runs["date"].iloc[0]
-            else:
-                return pd.NaT
-        else:
-            return df
+        index, ret = [], []
+        for k in range(cls.past_runs_):
+            date = latest_possible_date - pd.Timedelta(hours=cls.freq_update * k)
+            index.append(date)
+            urls = cls._get_urls(date=f"{date:%Y-%m-%dT%H}")
+            downloadable = are_downloadable(urls, return_date=return_date)
+            ret.append(downloadable)
+        return pd.Series(ret, index=index, name=f"{cls.__name__.lower()}")
